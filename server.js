886e1074947fb89ef5a122a08eba9ceb6df5cd05
Sricon <sricon@gmail.com>
2022-05-31 08:30:02 +0100

    Use env variable - WillItPass

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ function sleep(ms) {
 async function main() {
   while(true) {
     console.log("Microservices rock!");
+    console.log(`What do you think? Will it pass ? : ${process.env.WillItPass}`);
     await sleep(5000);
   }
 }
